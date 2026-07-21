7761609ec48315360324e3fccbceb8ff89de4f07
Zutah <zutahabraham0@gmail.com>
2026-07-21 12:01:17 +0000

    Use dynamic port for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const db = require('./database');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.use(express.json());
